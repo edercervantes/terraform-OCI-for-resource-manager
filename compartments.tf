42985cedb4e442fddb4f06c26db599347141b634
Eder Cervantes <cervanteseder15@gmail.com>
2020-03-03 16:30:06 -0800

    removing Demo compartment

--- compartments.tf
+++ compartments.tf
@@ -1,12 +1,7 @@
-//Creates Demo compartment in root, then python4dev in Demo compartment.
-resource "oci_identity_compartment" "Demo" {
-    compartment_id = "${var.compartment_ocid}"
-    description = "compartment for demo resources"
-    name = "Demo"
-}
+//Creates python4dev compartment in root compartment.
 
 resource "oci_identity_compartment" "python4dev" {
   name           = "python4dev"
   description    = "compartment to house python4dev resources"
-  compartment_id = "${oci_identity_compartment.Demo.id}"
+  compartment_id = "${var.compartment_ocid}"
 }
